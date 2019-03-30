a39f0e5708a3bd2f5835a39ad883840dc6a20a29
kelveenfabian <kelveenfabian@pursuit.org>
2019-03-30 13:32:14 -0400

    Added Zodiac and ZodiacWrapper classes within model package

---
+++ app/src/main/java/com/example/appfromscratch3/model/Zodiac.java
@@ -0,0 +1,19 @@
+package com.example.appfromscratch3.model;
+
+public class Zodiac {
+    private String name;
+    private String number;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
---
+++ app/src/main/java/com/example/appfromscratch3/model/ZodiacWrapper.java
@@ -0,0 +1,9 @@
+package com.example.appfromscratch3.model;
+
+public class ZodiacWrapper {
+    private Zodiac[] zodiac;
+
+    public Zodiac[] getZodiac() {
+        return zodiac;
+    }
+}
